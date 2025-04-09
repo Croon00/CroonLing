c6f9edfda2dfabd2d613f2c1b950011032260def
HyeonSeungmin <smhyun128@gmail.com>
2025-04-09 12:40:33 +0900

    Feat: OAuth2SuccessHandler의 컴포넌트 빠진거 추가

--- CroonLing-MSA/authservice/src/main/java/com/croonling/authservice/handler/OAuth2SuccessHandler.java
+++ CroonLing-MSA/authservice/src/main/java/com/croonling/authservice/handler/OAuth2SuccessHandler.java
@@ -8,9 +8,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class OAuth2SuccessHandler implements  AuthenticationSuccessHandler {
     private final JwtProvider jwtProvider;
     private final UserClient userClient;
