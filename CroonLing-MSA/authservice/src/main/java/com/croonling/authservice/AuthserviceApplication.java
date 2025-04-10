1c2bcc0d653e28b9cffe9108a9b862b13f6912c1
HyeonSeungmin <smhyun128@gmail.com>
2025-04-10 09:09:35 +0900

    Feat: auth-service의 client base-package 위치 설정

--- CroonLing-MSA/authservice/src/main/java/com/croonling/authservice/AuthserviceApplication.java
+++ CroonLing-MSA/authservice/src/main/java/com/croonling/authservice/AuthserviceApplication.java
@@ -2,10 +2,11 @@ package com.croonling.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.croonling.authservice.client")
 public class AuthserviceApplication {
 
 	public static void main(String[] args) {
