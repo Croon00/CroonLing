85bd3a084580f510c00edda01664b2fcbbc9df48
HyeonSeungmin <smhyun128@gmail.com>
2025-04-17 11:07:21 +0900

    Feat: 저장 안된 부분들 저장

--- CroonLing-py/database/mongodb.py
+++ CroonLing-py/database/mongodb.py
@@ -1,9 +1,10 @@
-from pymongo import MongoClient
+# from pymongo import MongoClient
 from config_loader import load_config
+from motor.motor_asyncio import AsyncIOMotorClient
 
 # ✅ MongoDB Atlas 연결
 config = load_config()
-client = MongoClient(config.get('MONGO_URI'))
+client = AsyncIOMotorClient(config.get('MONGO_URI'))
 mongo_db = client["croonling_db"]
 
 print("✅ MongoDB Atlas 연결 성공!")
--- CroonLing-py/service/kanji_service.py
+++ CroonLing-py/service/kanji_service.py
@@ -4,13 +4,12 @@ import logging
 
 class KanjiService:
     def __init__(self):
-        self.lyrics_db = LyricsDB()  # ✅ Lyrics 컬렉션 전용 DB
+        self.lyrics_db = LyricsDB()
         self.translator = ChatgptApi()
         self.logger = logging.getLogger(__name__)
 
-    def get_kanji_info(self, song_id):
-        """한자 정보 가져오기"""
-        kanji_info = self.lyrics_db.find_kanji_info(song_id)
+    async def get_kanji_info(self, song_id):
+        kanji_info = await self.lyrics_db.find_kanji_info(song_id)
         if kanji_info:
             self.logger.info(f"✅ 한자 정보 조회 성공 (ID: {song_id})")
             return kanji_info
@@ -18,16 +17,14 @@ class KanjiService:
         self.logger.warning(f"⚠️ 한자 정보 없음 (ID: {song_id})")
         return None
 
-    def save_kanji_info(self, song_id, kanji_info):
-        """한자 정보 저장"""
+    async def save_kanji_info(self, song_id, kanji_info):
         self.logger.info(f"📜 한자 정보 저장: 곡 ID '{song_id}'")
-        self.lyrics_db.upsert_kanji_info(song_id, kanji_info)
+        await self.lyrics_db.upsert_kanji_info(song_id, kanji_info)
         return f"곡 ID '{song_id}'의 한자 정보가 성공적으로 저장되었습니다."
 
-    def lyrics_to_kanji(self, song_id, lyrics):
-        """가사로부터 한자 정보 추출"""
-        kanji_info = self.translator.extract_kanji_info(lyrics)
+    async def lyrics_to_kanji(self, song_id, lyrics):
+        kanji_info = await self.translator.extract_kanji_info(lyrics)
         if kanji_info:
-            self.save_kanji_info(song_id, kanji_info)
+            await self.save_kanji_info(song_id, kanji_info)
             return kanji_info
         return None
