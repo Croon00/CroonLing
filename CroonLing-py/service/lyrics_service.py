f9d3136850510b889057289f923509db91e4c62f
HyeonSeungmin <smhyun128@gmail.com>
2025-05-07 14:25:53 +0900

    Fix: excludeSwitches 옵션 제거

--- CroonLing-py/service/lyrics_service.py
+++ CroonLing-py/service/lyrics_service.py
@@ -44,7 +44,9 @@ class LyricsService:
         options.add_argument("--window-size=1920,1080")
         options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36")
         options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_argument("--disable-infobars")
+        options.add_argument("--disable-popup-blocking")
+        options.add_argument("--disable-extensions")
 
         # ChromeDriver 초기화
         driver = None
